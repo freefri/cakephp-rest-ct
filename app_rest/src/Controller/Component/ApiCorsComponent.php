af74a52d9d61aba4e6f39cf75d69340e09bb7bbc
Adrian <freefri@freefri.es>
2021-11-08 12:38:35 +0100

    add basic Api2Controller

---
+++ app_rest/src/Controller/Component/ApiCorsComponent.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Component;
+
+use App\Controller\Api2Controller;
+use Cake\Controller\Component;
+use Cake\Controller\Controller;
+use Cake\Core\Configure;
+use Cake\Event\EventInterface;
+
+class ApiCorsComponent extends Component
+{
+    public static function load(Controller $controller)
+    {
+        $controller->loadComponent('ApiCors');
+    }
+
+    public function beforeFilter(EventInterface $event)
+    {
+        /** @var Api2Controller $controller */
+        $controller = $event->getSubject();
+        if ($controller) {
+            $response = $controller->getResponse();
+            $response->withDisabledCache();
+
+            $responseBuilder = $response->cors($controller->getRequest());
+
+            $allowedCors = Configure::read('Platform.Cors.AllowOrigin');
+            $currentOrigin = $_SERVER['HTTP_ORIGIN'] ?? '';
+            if ($currentOrigin && in_array($currentOrigin, $allowedCors)) {
+                $responseBuilder->allowOrigin([$currentOrigin])
+                    ->allowCredentials();
+            }
+            if ($controller->getRequest()->is('options')) {
+                $responseBuilder
+                    ->allowMethods(['POST', 'GET', 'PATCH', 'PUT', 'DELETE'])
+                    ->allowHeaders(['Authorization', 'Content-Type', 'Accept-Language'])
+                    ->maxAge(3600);
+                $response = $responseBuilder->build();
+                $controller->setResponse($response);
+                return $response;
+            }
+            $response = $responseBuilder->build();
+            $controller->setResponse($response);
+        }
+    }
+}
